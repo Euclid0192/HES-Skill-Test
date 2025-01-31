2517f3864d0f13a46ac7d872d7b17a7257beff40
Nam Nguyen <nguy1053@msu.edu>
2025-01-31 14:44:54 -0500

    Finishing critic page

--- app/components/CriticsPage/CriticsPage.jsx
+++ app/components/CriticsPage/CriticsPage.jsx
@@ -1,60 +1,67 @@
 import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { compose } from 'redux'
-import { getMovieReviews } from '../../resources/reviews/reviews.actions'
+import { getMovieCritics } from '../../resources/critics/critics.actions'
 import './CriticsPage.scss'
-
-const criticsData = [
-  {
-    display_name: 'A. O. Scott',
-    sort_name: 'A. O. Scott',
-    status: 'full-time',
-    bio:
-      'A. O. Scott joined The New York Times as a film critic in January 2000, and was named a chief critic in 2004. Previously, Mr. Scott had been the lead Sunday book reviewer for Newsday and a frequent contributor to Slate, The New York Review of Books, and many other publications. \n<br/><br/>\nIn the 1990s he served on the editorial staffs of Lingua Franca and The New York Review of Books. He also edited "A Bolt from the Blue and Other Essays," a collection by Mary McCarthy, which was published by The New York Review of Books in 2002. \n<br/><br/>\nMr. Scott was a finalist for the Pulitzer Prize in Criticism in 2010, the same year he served as co-host (with Michael Phillips of the Chicago Tribune) on the last season of "At the Movies," the syndicated film-reviewing program started by Roger Ebert and Gene Siskel.\n<br/><br/>\nA frequent presence on radio and television, Mr. Scott is Distinguished Professor of Film Criticism at Wesleyan University and the author of Better Living Through Criticism, forthcoming in 2016 from The Penguin Press. A collection of his film writing will be published by Penguin in 2017. \n<br/><br/>\nHe lives in Brooklyn with his family.',
-    seo_name: 'A-O-Scott',
-    multimedia: {
-      resource: {
-        type: 'image',
-        src:
-          'http://static01.nyt.com/images/2015/10/07/topics/ao-scott/ao-scott-articleInline.jpg',
-        height: 163,
-        width: 220,
-        credit: 'Earl Wilson/<br/>The New York Times',
-      },
-    },
-  },
-]
+import { getMovieReviews } from '../../resources/reviews/reviews.actions'
 
 const CriticsPage = props => {
   /// Fetch review list
   useEffect(() => {
+    props.getMovieCritics()
     props.getMovieReviews()
   }, [])
 
-  /// Destructure state of reviews
-  const { reviews } = props
+  /// Destructure state
+  const { critics, reviews } = props
+
+  /// Sort critics by name
+  const sortedCritics = critics.sort((c1, c2) =>
+    c1.display_name < c2.display_name ? -1 : 1
+  )
+  console.log('Sorted critics ', sortedCritics)
+
+  /// Function to get total number of Reviews written by a critic
+  const getCountReviewsWritten = critic => {
+    return reviews.filter(
+      review =>
+        review.byline.toLowerCase() === critic.display_name.toLowerCase()
+    ).length
+  }
+  /// Function to get total number of Critics Pick of a critic
+  const getCountCriticsPick = critic => {
+    return reviews.filter(
+      review =>
+        review.critics_pick &&
+        review.byline.toLowerCase() === critic.display_name.toLowerCase()
+    ).length
+  }
 
   return (
-    <div className='critic-container'>
+    <div className="critic-container">
       <h1>Critics</h1>
       <ul className="critic-list">
-        {criticsData.map((critic, index) => (
+        {sortedCritics.map((critic, index) => (
           <div key={index} className="critic-item">
-            {/* {critic.multimedia?.resource?.src && (
+            {critic.multimedia && critic.multimedia.resource && (
               <img
                 src={critic.multimedia.resource.src}
                 alt={critic.display_name}
+                width={critic.multimedia.resource.width}
+                height={critic.multimedia.resource.height}
               />
-            )} */}
+            )}
             <h2>{critic.display_name}</h2>
-            <p>Reviews Written: {critic.reviews_written}</p>
-            <p>Critic's Pick: {critic.critics_pick}</p>
-            {critic.bio && (
+            <p>Reviews Written: {getCountReviewsWritten(critic)}</p>
+            <p>Critic's Pick: {getCountCriticsPick(critic)}</p>
+            {critic.bio ? (
               <p>
                 {critic.bio.length > 200
                   ? critic.bio.substring(0, 200) + '...'
                   : critic.bio}
               </p>
+            ) : (
+              <p className="italic">No information about this critic.</p>
             )}
           </div>
         ))}
@@ -65,11 +72,13 @@ const CriticsPage = props => {
 
 const mapStateToProps = (state, ownProps) => {
   return {
-    reviews: state.resources.reviews ? state.resources.reviews.data : [], // Access reviews under resources
+    critics: state.resources.critics ? state.resources.critics.critics : [], // Access critics under resources
+    reviews: state.resources.reviews ? state.resources.reviews.data : [],
   }
 }
 
 const mapDispatchToProps = dispatch => ({
+  getMovieCritics: () => dispatch(getMovieCritics()),
   getMovieReviews: () => dispatch(getMovieReviews()),
 })
 
